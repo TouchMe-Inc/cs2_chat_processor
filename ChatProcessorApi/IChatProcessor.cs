c86437a94127b727d7e2c2490022e2ce3d7865a4
Dmitry Pavlukov <dmitry.pavlukov@gmail.com>
2024-05-30 19:00:36 +0300

    Add comments

--- ChatProcessorApi/IChatProcessor.cs
+++ ChatProcessorApi/IChatProcessor.cs
@@ -11,11 +11,27 @@ public interface IChatProcessor
 
     delegate void MessagePostCallback(CCSPlayerController sender, string name, string message, List<CCSPlayerController> recipients, int flags);
 
+    /// <summary>
+    /// Registers a chat event handler.
+    /// </summary>
+    /// <param name="handler">The event handler to register.</param>
     void RegisterHandlerPre(MessagePreCallback handler);
 
+    /// <summary>
+    /// De-registers a chat event handler.
+    /// </summary>
+    /// <param name="handler">The event handler to deregister.</param>
     void DeregisterHandlerPre(MessagePreCallback handler);
 
+    /// <summary>
+    /// Registers a chat event handler.
+    /// </summary>
+    /// <param name="handler">The event handler to register.</param>
     void RegisterHandlerPost(MessagePostCallback handler);
 
+    /// <summary>
+    /// De-registers a chat event handler.
+    /// </summary>
+    /// <param name="handler">The event handler to deregister.</param>
     void DeregisterHandlerPost(MessagePostCallback handler);
 }
