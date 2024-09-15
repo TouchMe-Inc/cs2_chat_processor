813edd371fad4f93d8841b3f8acc217b2fc0fd30
Dmitry Pavlukov <89782512+TouchMe-Inc@users.noreply.github.com>
2024-09-15 22:43:26 +0300

    Fix format template

--- ChatProcessor/ChatProcessor.cs
+++ ChatProcessor/ChatProcessor.cs
@@ -168,7 +168,6 @@ public class ChatProcessor : BasePlugin
                         else
                         {
                             withPlace = true;
-                            formatMessage = LangKey.CHAT_T;
                             formatMessage = LangKey.CHAT_T_LOC;
                         }
                     }
